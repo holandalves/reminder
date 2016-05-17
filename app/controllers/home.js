7723fb51e0d0cc437723d0e292ba090fbc8266a4
Aldenor Junior <aldenor.junior@tce.ro.gov.br>
2016-05-17 01:57:28 -0400

    Added new home view and correction on controller method

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,7 +2,7 @@ module.exports = function() {
 	var controller = {};
 
 	controller.index = function(req, res) {
-		res.render('index', {nome: 'Reminder'});
+		res.render('home', {nome: 'Reminder'});
 	};
 
 	controller.ola = function() {
